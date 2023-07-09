749c5e4d745c498953c1e40cc2079dcecec60151
Samuel Chen <samuel.chen@akunacapital.com>
2023-07-09 18:14:14 +1000

    feat: add content to body, change default font color

--- src/components/folder/Footer.tsx
+++ src/components/folder/Footer.tsx
@@ -1,6 +1,10 @@
-
 export const Footer = () => {
-    return <footer className="flex w-full h-[80px] bg-white border-t justify-center items-center">
-        <p className="text-center italic">Made with <span className="not-italic">❤</span>️ in Sydney.<br/>© 2023 Broccoli & Co. All rights reserved.</p>
+  return (
+    <footer className="flex w-full h-[80px] bg-white border-t justify-center items-center">
+      <p className="text-center italic">
+        Made with <span className="not-italic">❤</span>️ in Sydney.
+        <br />© 2023 Broccoli & Co. All rights reserved.
+      </p>
     </footer>
-}
+  );
+};
