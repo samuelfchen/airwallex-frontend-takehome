5ea50e2f5f0248ee2b79f7827f90203bc6d21ea2
Samuel Chen <samuel.chen@akunacapital.com>
2023-07-09 17:54:36 +1000

    feat: add and style header, body, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Body } from "./components/body/Body";
+import { Footer } from "./components/folder/Footer";
+import { Header } from "./components/header/Header";
+
 function App() {
 	return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col h-screen justify-between">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
 	);
 }
 
---
+++ src/components/body/Body.tsx
@@ -0,0 +1,7 @@
+export const Body = () => {
+    return <div className="w-full flex justify-center items-center">
+        <div>
+            Body goes here
+        </div>
+    </div>
+}
