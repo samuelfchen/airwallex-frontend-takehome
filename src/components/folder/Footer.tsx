4931e1569455b6f672c5cabd161d05511159222f
Samuel Chen <samuel.chen@akunacapital.com>
2023-07-09 17:54:36 +1000

    feat: add and style header, body, footer

---
+++ src/components/folder/Footer.tsx
@@ -0,0 +1,6 @@
+
+export const Footer = () => {
+    return <footer className="flex w-full h-[80px] bg-white border-t justify-center items-center">
+        <p className="text-center italic">Made with <span className="not-italic">❤</span>️ in Sydney.<br/>© 2023 Broccoli & Co. All rights reserved.</p>
+    </footer>
+}
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="w-full h-[80px] bg-white border-b flex items-center">
+      <h1 className="ml-20 text-3xl font-medium uppercase">Broccoli & Co.</h1>
+    </div>
+  );
+};
