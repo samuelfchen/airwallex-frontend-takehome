b8b276d6e074dc0023cc592c043553cd627e3e89
Samuel Chen <samuel.chen@akunacapital.com>
2023-07-10 10:49:52 +1000

    chore: add daisyui source to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+  ],
   theme: {
     extend: {},
   },
