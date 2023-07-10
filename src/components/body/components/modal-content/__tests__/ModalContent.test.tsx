8203454e1e4440f6b4f96e8b1cad2b8befe214f2
Samuel Chen <samuel.chen@akunacapital.com>
2023-07-11 00:14:42 +1000

    test: complete modal content test coverage

--- src/components/body/components/modal-content/__tests__/ModalContent.test.tsx
+++ src/components/body/components/modal-content/__tests__/ModalContent.test.tsx
@@ -1,5 +1,8 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+
 import { ModalContent } from "../ModalContent";
+import { server } from "../components/signup/__tests__/mocks/server";
+import userEvent from "@testing-library/user-event";
 
 describe("ModalContent", () => {
   it("renders signup button", () => {
@@ -10,5 +13,35 @@ describe("ModalContent", () => {
     render(<ModalContent />);
 
     expect(screen.getByTestId("signup-form")).toBeVisible();
+  });
+
+  it("switches to success page upon successful signup", async () => {
+    server.listen();
+
+    render(<ModalContent />);
+    const submit = screen.getByRole("button")!;
+    const fullName = screen.getByTestId("fullName-input");
+    const email = screen.getByTestId("email-input");
+    const confirmEmail = screen.getByTestId("confirmEmail-input");
+
+    // click button, and assert that error messages are all displayed
+
+    await userEvent.type(fullName.querySelector("input")!, "John Doe");
+    await userEvent.type(
+      email.querySelector("input")!,
+      "john.doe@airwallex.com"
+    );
+    await userEvent.type(
+      confirmEmail.querySelector("input")!,
+      "john.doe@airwallex.com"
+    );
+
+    await userEvent.click(submit);
+
+    await waitFor(() => {
+      expect(screen.getByTestId("success-modal")).not.toBeNull();
+    });
+
+    server.close();
   });
 });
